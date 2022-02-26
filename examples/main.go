9ac4b2b63bd5f2e2b1fc0a8106265bdbf09bee60
yaoming00 <l1ng14@foxmail.com>
2022-02-26 17:20:47 +0800

    feat: add route config

--- examples/main.go
+++ examples/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"gym"
 	"log"
 	"net/http"
 )
 
 func main() {
-	r := new(gym.Engine)
-	err := http.ListenAndServe(":9099", r)
+	r := gym.New()
+
+	r.GET("/hello", func(w http.ResponseWriter, r *http.Request) {
+		for k, v := range r.Header {
+			log.Printf("[%q]: %q\n", k, v)
+			fmt.Fprintf(w, "[%q]: %q\n", k, v)
+		}
+	})
+
+	r.POST("/post", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Path: %q\n", r.URL.Path)
+		fmt.Fprintf(w, "Path: %q\n", r.URL.Path)
+	})
+
+	err := r.Run(":9099")
 	if err != nil {
 		log.Fatalln("Run server ERROR:", err)
 	}
--- gym.go
+++ gym.go
@@ -6,20 +6,55 @@ import (
 	"net/http"
 )
 
-type Engine struct{}
+type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+
+const (
+	GET    = "GET"
+	POST   = "POST"
+	PUT    = "PUT"
+	DELETE = "DELETE"
+)
+
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (e *Engine) addRoute(method, path string, handler HandlerFunc) {
+	key := method + path
+	e.router[key] = handler
+}
+
+func (e *Engine) GET(path string, handler HandlerFunc) {
+	e.addRoute(GET, path, handler)
+}
+
+func (e *Engine) POST(path string, handler HandlerFunc) {
+	e.addRoute(POST, path, handler)
+}
+
+func (e *Engine) PUT(path string, handler HandlerFunc) {
+	e.addRoute(PUT, path, handler)
+}
+
+func (e *Engine) DELETE(path string, handler HandlerFunc) {
+	e.addRoute(DELETE, path, handler)
+}
+
+func (e *Engine) Run(addr string) error {
+	log.Println("Server listening at ", addr)
+	return http.ListenAndServe(addr, e)
+}
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		log.Printf("Path: %q\n", r.URL.Path)
-		fmt.Fprintf(w, "Path: %q\n", r.URL.Path)
-	case "/hello":
-		for k, v := range r.Header {
-			log.Printf("[%q]: %q\n", k, v)
-			fmt.Fprintf(w, "[%q]: %q\n", k, v)
-		}
-	default:
+	if handler, ok := e.router[r.Method+r.URL.Path]; ok {
+		handler(w, r)
+	} else {
 		log.Printf("404 Not Found\n")
-		fmt.Fprintf(w, "404 Not Found\n")
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "404 Not Found %q %q\n", r.Method, r.URL.Path)
 	}
 }
